d4b778f4276be8a1a5d832674c3d531049d0d501
nihalUzunyayla <nihaluzunyayla@gmail.com>
2023-12-27 18:20:50 +0300

    Donation Activity

--- app/src/main/java/com/example/pawrescue/DonationActivity.java
+++ app/src/main/java/com/example/pawrescue/DonationActivity.java
@@ -1,52 +1,19 @@
 package com.example.pawrescue;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-
-import com.example.pawrescue.ui.main_menu.MainMenuFragment;
-import com.example.pawrescue.ui.home.HomeFragment;
-import com.example.pawrescue.ui.settings.SettingsFragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.example.pawrescue.databinding.ActivityMainBinding;
 
 public class DonationActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+    private Menu menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donation);
-
-       /* BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment selectedFragment = null;
-
-                if (item.getItemId() == R.id.navigation_home) {
-                    selectedFragment = new HomeFragment();
-                } else if (item.getItemId() == R.id.navigation_main_menu) {
-                    selectedFragment = new MainMenuFragment();
-                } else if (item.getItemId() == R.id.navigation_settings) {
-                    selectedFragment = new SettingsFragment();
-                }
-
-                if (selectedFragment != null) {
-                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                    transaction.replace(R.id.activity_donation, selectedFragment);
-                    transaction.commit();
-                }
-
-                return true;
-            }
-        });*/
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
