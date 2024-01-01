3ffccde119c6d04899df9ba4f33283fd6ba55f80
EdanurSARIKAYA <edanrsrky98@gmail.com>
2024-01-01 13:47:24 +0300

    Fixed navigation for back button.

--- app/src/main/java/com/example/pawrescue/AddPetActivity.java
+++ app/src/main/java/com/example/pawrescue/AddPetActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -234,7 +235,16 @@ public class AddPetActivity extends AppCompatActivity {
             }
         }
     }
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater mif = getMenuInflater();
