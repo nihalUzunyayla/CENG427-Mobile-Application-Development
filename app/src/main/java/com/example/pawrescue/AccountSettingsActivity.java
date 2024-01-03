e5d0d1f9e4ac0da8ff746db34c55e9e9ce789f78
EdanurSARIKAYA <edanrsrky98@gmail.com>
2024-01-03 15:58:24 +0300

    Updated Settings

--- app/src/main/java/com/example/pawrescue/AccountSettingsActivity.java
+++ app/src/main/java/com/example/pawrescue/AccountSettingsActivity.java
@@ -1,6 +1,6 @@
-
 package com.example.pawrescue;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
@@ -8,15 +8,21 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import java.util.Locale;
 
 public class AccountSettingsActivity extends AppCompatActivity {
 
     private static final String PREF_LANGUAGE_KEY = "language_preference";
+    private static final String PREF_DARK_MODE_KEY = "dark_mode_preference";
     private static final String DEFAULT_LANGUAGE = "en";
+    TextView bottomTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +31,37 @@ public class AccountSettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_account_settings);
 
         RadioGroup radioGroupLanguage = findViewById(R.id.radioGroupLanguage);
+        Switch switchDarkMode = findViewById(R.id.switchDarkMode);
+        SeekBar fontSizeSeekBar = findViewById(R.id.fontSizeSeekBar);
+        bottomTextView = findViewById(R.id.bottomTextView);
 
-        // Dil tercihini kontrol et
+        fontSizeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                switch (progress) {
+                    case 0:
+                        bottomTextView.setTextSize(16);
+                        break;
+                    case 1:
+                        bottomTextView.setTextSize(24);
+                        break;
+                    case 2:
+                        bottomTextView.setTextSize(32);
+                        break;
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // SeekBar değişmeye başladığında yapılacak işlemler
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // SeekBar değişimi durduğunda yapılacak işlemler
+            }
+        });
+
         String currentLanguage = getLanguagePreference();
         if (currentLanguage.equals("en")) {
             ((RadioButton) findViewById(R.id.radioButtonEnglish)).setChecked(true);
@@ -34,7 +69,6 @@ public class AccountSettingsActivity extends AppCompatActivity {
             ((RadioButton) findViewById(R.id.radioButtonTurkish)).setChecked(true);
         }
 
-        // Dil seçimi değiştiğinde tetiklenecek olay dinleyicisi
         radioGroupLanguage.setOnCheckedChangeListener((group, checkedId) -> {
             if (checkedId == R.id.radioButtonEnglish) {
                 setLanguagePreference("en");
@@ -42,9 +76,28 @@ public class AccountSettingsActivity extends AppCompatActivity {
                 setLanguagePreference("tr");
             }
 
-            // Uygulamayı tekrar başlatarak dil değişikliklerini uygula
             recreate();
         });
+
+        switchDarkMode.setChecked(getDarkModePreference());
+
+        switchDarkMode.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            setDarkModePreference(isChecked);
+
+            if (isChecked) {
+                enableDarkMode();
+            } else {
+                disableDarkMode();
+            }
+        });
+    }
+
+    private void enableDarkMode() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+    }
+
+    private void disableDarkMode() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
     }
 
     private void setAppLanguage() {
@@ -57,6 +110,7 @@ public class AccountSettingsActivity extends AppCompatActivity {
 
         Resources resources = getResources();
         resources.updateConfiguration(config, resources.getDisplayMetrics());
+
     }
 
     private String getLanguagePreference() {
@@ -69,5 +123,15 @@ public class AccountSettingsActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = prefs.edit();
         editor.putString(PREF_LANGUAGE_KEY, language);
         editor.apply();
+    }
+    private void setDarkModePreference(boolean isDarkMode) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean(PREF_DARK_MODE_KEY, isDarkMode);
+        editor.apply();
+    }
+    private boolean getDarkModePreference() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        return prefs.getBoolean(PREF_DARK_MODE_KEY, false); // Varsayılan değer false olarak ayarlandı.
     }
 }
